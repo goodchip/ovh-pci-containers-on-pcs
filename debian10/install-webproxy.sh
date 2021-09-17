c518bb5f4d3de7932247b63e7eb5c986117d6949
Sylvain Vauclin <4836635+goodchip@users.noreply.github.com>
2021-09-17 14:11:47 +0200

    Update install-webproxy.sh

--- debian10/install-webproxy.sh
+++ debian10/install-webproxy.sh
@@ -8,24 +8,28 @@
 #                                            #
 ##############################################
 
-# define default variables:
-DEFAULT_EMAIL="email";
-DEFAULT_IP=0.0.0.0;
-DEFAULT_CONTAINERS_PATH="/mnt/containers/"
-DEFAULT_CONTAINERS_NAME="docker-proxy"
+ENV_PATH="../ovh-pci-containers-on-pcs/.env"
 
 # define help function:
 help()
 {
    echo ""
    echo "usage: $0 [-e <email>] [-i <ip>] [-p <path>] [-n <name>] [-h]"
-   echo -e "\t-e : set email for cetificat (default: $DEFAULT_EMAIL)"
-   echo -e "\t-i : set public ip to listen (default: $DEFAULT_IP)"
-   echo -e "\t-p : set path of container to install (default: $DEFAULT_CONTAINERS_PATH)"
-   echo -e "\t-n : set name of container to install (default: $DEFAULT_CONTAINERS_NAME)"
+   echo -e "\t-e : set email for cetificat (default: $DEFAULT_CONTAINER_WEBPROXY_EMAIL)"
+   echo -e "\t-i : set public ip to listen (default: $DEFAULT_CONTAINER_WEBPROXY_IP)"
+   echo -e "\t-p : set path of container to install (default: $DEFAULT_CONTAINER_WEBPROXY_PATH)"
+   echo -e "\t-n : set name of container to install (default: $DEFAULT_CONTAINER_WEBPROXY_NAME)"
    echo -e "\t-h : display this help"
    exit 1
 }
+
+# get default environnement variables:
+if ! [ -f "$ENV_PATH" ]
+then
+  echo "Must be run on project root directory."
+  exit 1
+fi
+export $(cat "$ENV_PATH" | sed 's/#.*//g' | xargs)
 
 # scan arguments:
 while getopts "e:i:p:n:h" opt
@@ -48,25 +52,25 @@ fi
 # update disk name defaults if set:
 if [ -z "$EMAIL" ]
 then
-   EMAIL=$DEFAULT_EMAIL;
+   EMAIL=$DEFAULT_CONTAINER_WEBPROXY_EMAIL;
 fi
 
 # update partition number defaults if set:
 if [ -z "$IP" ]
 then
-   IP=$DEFAULT_IP;
+   IP=$DEFAULT_CONTAINER_WEBPROXY_IP;
 fi
 
 # update container path defaults if set:
 if [ -z "$CONTAINERS_PATH" ]
 then
-   CONTAINERS_PATH=$DEFAULT_CONTAINERS_PATH;
+   CONTAINERS_PATH=$DEFAULT_CONTAINER_WEBPROXY_PATH;
 fi
 
 # update container name defaults if set:
 if [ -z "$CONTAINERS_NAME" ]
 then
-   CONTAINERS_NAME=$DEFAULT_CONTAINERS_NAME;
+   CONTAINERS_NAME=$DEFAULT_CONTAINER_WEBPROXY_NAME;
 fi
 
 # start installation:

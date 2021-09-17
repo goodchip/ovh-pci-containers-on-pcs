76bfcbc0cbd84aed74c149474586d45c4cd0c6c3
Sylvain Vauclin <4836635+goodchip@users.noreply.github.com>
2021-09-17 14:05:26 +0200

    Update install-pcs.sh

--- debian10/install-pcs.sh
+++ debian10/install-pcs.sh
@@ -8,30 +8,31 @@
 #                                            #
 ##############################################
 
-# define default variables:
-DEFAULT_DISK_NAME="/dev/sdb";
-DEFAULT_PART_NUMBER=1;
-DEFAULT_MOUNT_PATH="/mnt/containers/"
+ENV_PATH="../ovh-pci-containers-on-pcs/.env"
 
 # define help function:
 help()
 {
    echo ""
-   echo "usage: $0 [-d <disk>] [-p <partition>] [-h]"
-   echo -e "\t-d : set disk to install (default: $DEFAULT_DISK_NAME)"
-   echo -e "\t-p : set partition to install (default: $DEFAULT_PART_NUMBER)"
-   echo -e "\t-m : set mount path to install (default: $DEFAULT_MOUNT_PATH)"
+   echo "usage: $0 [-c <version>] [-h]"
+   echo -e "\t-c : set docker-compose version to install (optional)"
    echo -e "\t-h : display this help"
    exit 1
 }
 
+# get default environnement variables:
+if ! [ -f "$ENV_PATH" ]
+then
+  echo "Must be run on project root directory."
+  exit 1
+fi
+export $(cat "$ENV_PATH" | sed 's/#.*//g' | xargs)
+
 # scan arguments:
-while getopts "d:p:m:h" opt
+while getopts "c:h" opt
 do
    case "$opt" in
-      d ) DISK_NAME="$OPTARG" ;;
-      p ) PART_NUMBER="$OPTARG" ;;
-      m ) MOUNT_PATH="$OPTARG" ;;
+      c ) PCI_DOCKER_COMPOSE_VERSION="$OPTARG" ;;
       h ) help ;;
    esac
 done
@@ -42,47 +43,23 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# update disk name defaults if set:
-if [ -z "$DISK_NAME" ]
+# get docker-compose version if set
+if [ -z "$PCI_DOCKER_COMPOSE_VERSION" ]
 then
-   DISK_NAME=$DEFAULT_DISK_NAME;
+   PCI_DOCKER_COMPOSE_VERSION=$DEFAULT_PCI_DOCKER_COMPOSE_VERSION;
 fi
 
-# update partition number defaults if set:
-if [ -z "$PART_NUMBER" ]
-then
-   PART_NUMBER=$DEFAULT_PART_NUMBER;
-fi
+# install docker:
+echo "Installing docker...";
+apt install docker.io -y
+systemctl start docker
+systemctl enable docker
+docker version
 
-# update mount path defaults if set:
-if [ -z "$MOUNT_PATH" ]
-then
-   MOUNT_PATH=$DEFAULT_MOUNT_PATH;
-fi
-
-# start installation:
-echo "Installing $DISK_NAME$PART_NUMBER on $MOUNT_PATH ...";
-
-# partionning additional disk:
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk "$DISK_NAME"
-  o # clear the in memory partition table
-  n # new partition
-  p # primary partition
-  $PART_NUMBER # partition number $PART_NUMBER
-    # default - start at beginning of disk 
-    # default - end at bottom of disk
-  w # write the partition table
-  q # and we're done
-EOF
-
-# format partition of additional disk:
-mkfs.ext4 "$DISK_NAME$PART_NUMBER"
-
-# automount partition of additional disk:
-mkdir "$MOUNT_PATH"
-mount "$DISK_NAME$PART_NUMBER" "$MOUNT_PATH"
-DISK_UUID="$(blkid -s UUID -o value $DISK_NAME$PART_NUMBER)"
-echo "UUID="$DISK_UUID"    $MOUNT_PATH    ext4    nofail    0    0" | tee -a /etc/fstab
+# install docker-compose:
+echo "Installing docker-compose" $PCI_DOCKER_COMPOSE_VERSION "on" $DEFAULT_PCS_DOCKER_COMPOSE_PATH "...";
+curl -L "https://github.com/docker/compose/releases/download/$PCI_DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o $DEFAULT_PCS_DOCKER_COMPOSE_PATH
+chmod +x $DEFAULT_PCS_DOCKER_COMPOSE_PATH
 
 # end:
 echo "Installation finished.";
